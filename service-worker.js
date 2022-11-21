476a8a0b762d798172e6e62a501a42dd234cc22f
Florian Häcker <haecker.florian@gmail.com>
2022-11-21 17:42:28 +0100

    Updates

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const cacheName = "cache-v1";
+const cacheURLS = [
+    "/workout-timer/index.html"
+];
+
+this.addEventListener("install", (e) => {
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => cache.addAll(cacheURLS)),
+    );
+});
+  
+this.addEventListener("fetch", (e) => {
+    console.log(e.request.url);
+    e.respondWith(
+        caches.match(e.request).then((response) => response || fetch(e.request)),
+    );
+});
+  
